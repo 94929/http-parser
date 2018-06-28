8fadd2c27df413ca4a07993506851d635c19db7f
Jinsung Ha <jsung5381@naver.com>
2018-06-28 20:10:03 +0100

    Add path to data/

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        // HeaderParser HP = new HeaderParser();
-        // HP.readHeader("multipart.txt");
-        // HP.writeHeader("parsed_multipart.txt");
+        HeaderParser HP = new HeaderParser();
+        HP.readHeader("data/multipart.txt");
+        HP.writeHeader("parsed_multipart.txt");
 
-        // DataParser MPDP = new MultiPartDataParser();
-        // MPDP.readData("multipart.txt");
-        // MPDP.writeData("parsed_multipart.txt");
+        DataParser MPDP = new MultiPartDataParser();
+        MPDP.readData("data/multipart.txt");
+        MPDP.writeData("parsed_multipart.txt");
 
         DataParser UEDP = new UrlEncodedDataParser();
-        UEDP.readData("urlencoded.txt");
+        UEDP.readData("data/urlencoded.txt");
         UEDP.writeData("parsed_urlencoded.txt");
     }
 }
